8e031f47c1683887458756ea2827984441d1b7a6
debian-projects <debianprojetos@gmail.com>
2025-01-15 03:14:17 -0300

    Create index.js

---
+++ include/class/Send/index.js
@@ -0,0 +1,16 @@
+const { Sock } = require("../../index.js");
+
+class Send extends Sock {
+
+    public sock; 
+
+  constructor() {
+    super();
+    this.sock = sock;
+  }
+
+
+
+  
+} 
+module.exports = Send;
